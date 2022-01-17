1656ccb8a8508dafd4d5ca545c1a5ddbc0ad21a4
steve <steve.hummingbird@yandex.com>
2022-01-17 22:46:43 +0100

    ember uprade, dependency upgrade & migrate tests where necessary, remove outdated dependencies without updates available

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,7 +3,7 @@
 
 const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
-module.exports = function(defaults) {
+module.exports = function (defaults) {
   let app = new EmberAddon(defaults, {
     'ember-cli-babel': {
       includePolyfill: true,
@@ -17,5 +17,12 @@ module.exports = function(defaults) {
     behave. You most likely want to be modifying `./index.js` or app's build file
   */
 
-  return app.toTree();
+  const { maybeEmbroider } = require('@embroider/test-setup');
+  return maybeEmbroider(app, {
+    skipBabel: [
+      {
+        package: 'qunit',
+      },
+    ],
+  });
 };
